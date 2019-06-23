03cdd4a7aed4621aaf3123439c3d414c1e9e5ad5
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-06-22 23:20:34 -0700

    closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,23 @@
 var wins = 0;
 var losses = 0;
-var guessesLeftLog = 10;
+var guessesLeftLog = 0;
 var guessesFarLog = [];
 var letters = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"]
 var lettersLength = letters.length; 
 var answer = [];
+
+
 //.push adds something to the end of the array
+function updateDom() {
 var winstext = document.getElementById('wins-text');
 var lossestext = document.getElementById('losses-text');
 var guessesLefttext = document.getElementById('guessesLeftLog');
 var guessesFartext = document.getElementById('guessesFarLog');
+}
 
-if (guessesFartext === answer) {
-    win();
-} else if (guessesLeftLog - 1 == 0) {lost ();}
+function startOver() {
+    guessesLeftLog = 10;
+}
 
 
 for (var i=0; i < length; i++) {
@@ -22,9 +26,17 @@ for (var i=0; i < length; i++) {
 
 
 document.onkeyup = function(event) {
-    guessesFarLog.textContent = event.key;
-    console.log(guessesFartext);
+    console.log(guessesFarLog);
     var guessesFartext = document.getElementById('guessesFarLog');
+    guessesFartext.textContent = event.key;
+    if (guessesFartext === answer) {
+        win();
+    } else if (guessesLeftLog - 1 == 0) {
+        lost ();
+    } 
+    else {
+       fail(guessesFarText);
+    }
 }
 
 
