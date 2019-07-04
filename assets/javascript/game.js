7daee6bf562149dbfc0c2d9d14248612e59cee51
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-04 08:59:59 -0700

    point totals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,59 @@
-var winsLog = 0;
-var lossesLog = 0;
-var guessesLeftLog = 10;
+var wins = 0;
+var losses = 0;
+var guessesLeftLog = 11;
 var guessesFarLog = [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lettersLength = letters.length; 
+var lettersLength = letters.length;
+
 var startOver = function () {
-    guessesLeftLog = 10;
-    guessesFarLog = [];
-    guessesFartext ();
-    letters ();
-    guessesLefttext ();
+    guessesLeftLog = 11;
+    guessesFarLog = [''];
+    compChoiceFunction();
+}
+
+
+
+var guessesLeftText = 
+    document.querySelector("#guessesLeftHtml").innerHTML = guessesLeftLog;
+
+
+
+var guessesText = document.querySelector('#guessesFarLog');
+//winstext.innerHTML = document.querySelector('#winsLog');
+//lossestext.innerHTML = document.querySelector('#lossesLog');
+
+var compChoiceText = letters[Math.floor(Math.random() * letters.length)];
+var compChoiceFunction = function () {
+    compChoiceText;
 };
 
+console.log('The answer is ' + compChoiceText + '.');
+console.log(guessesLeftLog)
 
-var winstext = document.getElementById('winsLog');
-var lossestext = document.getElementById('lossesLog');
-var guessesLefttext = document.querySelector('#guessesLeftLog');
-var guessesFartext = document.querySelector('#guessesFarLog');
-winstext.innerHTML = document.querySelector('#winsLog') ;
-lossestext.innerHTML = document.querySelector('#lossesLog'); 
-guessesLefttext.innerHTML = guessesLeftLog ;
-var compChoice = letters[Math.floor(Math.random() * letters.length)]
+document.onkeyup = function (event) {
+    guessesLeftText--;
+    guessesLeftLog.textContent = guessesLeftText
+    guessesText.textContent = guessesText.textContent + ' ' + event.key;
 
-console.log('The answer is ' + compChoice + '.' )
-
-
-document.onkeyup = function(event) {
-    console.log(guessesFarLog);
-    guessesLeftLog--;
-    var guessesFartext = document.getElementById('guessesFarLog');
-    guessesFartext.textContent = guessesFartext.textContent + ' ' + event.key;
-    console.log(letters)
-    if (guessesFartext === compChoice) {
-        winsLog++;
-        document.getElementById("winsLog").textContent = winsLog;
-        startOver ();
+    if (event.key === compChoiceText) {
+        wins++;
+        document.querySelector("#wins").textContent = wins;
+        startOver();
+        guessesLeftLog = 11;
+        guessesFarLog = [''];
+        compChoiceFunction();
     };
-    
-    if (guessesLeftLog === 0) {
-        lossestext++;
 
-    }
-}; 
+    if (guessesLeftText === 0) {
+        losses++;
+        document.querySelector('#losses').textContent = losses;
+        startOver();
+    };
 
-function startOver() {
-    guessesLefttext = 0;
-    guessesFartext -1;
-    guessesFartext [" "];
-}
-;
+    console.log(guessesText);
+    startOver();
+};
+
 
 //function updateDom() { 
 //document.getElementById("wins").innerHTML = wins 
